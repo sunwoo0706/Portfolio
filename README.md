c2ed670111565a99ad05b21ed6a3fa3e97261d0e
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-03-14 19:24:21 +0900

    🎉 init project

---
+++ README.md
@@ -0,0 +1,3 @@
+## Portfolio
+
+sunwoo's portfolio
---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import type { AppProps } from 'next/app';
+import emotionReset from 'emotion-reset';
+import { Global, css } from '@emotion/react';
+
+export function GlobalStyle() {
+  return (
+    <Global
+      styles={css`
+        ${emotionReset}
+        *, *::after, *::before {
+          box-sizing: border-box;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-smoothing: antialiased;
+        }
+      `}
+    />
+  );
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
