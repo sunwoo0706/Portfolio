53212ccf7384b6d5a06d156cf76b4a56443069f4
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-03-17 11:29:37 +0900

    Fix 2 render err and remove comment

--- src/components/Star/Star.tsx
+++ src/components/Star/Star.tsx
@@ -3,20 +3,24 @@ import styled from '@emotion/styled';
 import { StarElementProps, generateStar } from './container';
 import { displayType } from 'shared/Type';
 import { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
 
 const StarLayer = styled.div<StarElementProps>`
-  width: ${({ size }) => size}px;
-  height: ${({ size }) => size}px;
+  ${({ n, size, display }) => css`
+    width: ${size}px;
+    height: ${size}px;
+    ${generateStar(n, display)}
+  `}
   background: transparent;
-  ${({ n, display }) => generateStar(n, display)};
   &:after {
     content: ' ';
     position: absolute;
-    top: 2000px;
-    width: ${({ size }) => size}px;
-    height: ${({ size }) => size}px;
     background: transparent;
-    ${({ n, display }) => generateStar(n, display)};
+    ${({ n, size, display }) => css`
+      width: ${size}px;
+      height: ${size}px;
+      ${generateStar(n, display)}
+    `}
   }
 `;
 
--- src/components/Star/container.tsx
+++ src/components/Star/container.tsx
@@ -9,28 +9,17 @@ export interface StarElementProps {
   display: displayType;
 }
 
-/**
- * css의 box-shadow 속성을 이용해 배경에 쓰이는 별 생성기
- * @param n 별의 개수
- * @param display 현재 뷰포트의 크기
- */
 export const generateStar = (
   n: number,
   display: displayType,
 ): SerializedStyles => {
   const color: string = pallete.scheme.white;
 
-  /**
-   * @returns 뷰포트 크기 안에서 랜덤한 별 좌표
-   */
   const randomXY = (): string =>
     `${Math.floor(Math.random() * display?.width)}px ${Math.floor(
       Math.random() * display?.height,
     )}px ${color}`;
 
-  /**
-   * @returns 별 생성을 위한 `box-shadow` 문자열
-   */
   const generateBoxShadowVal = (): string => {
     let boxShadow: string = '';
 
